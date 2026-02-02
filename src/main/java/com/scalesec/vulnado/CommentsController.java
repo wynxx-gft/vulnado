b87cea3c0246198e10227c188216625249db40b5
Julio Arruda <julio.arruda@outlook.com>
2026-02-02 15:09:01 -0300

    [GFTCodeFixer]: Update on src/main/java/com/scalesec/vulnado/CommentsController.java

--- src/main/java/com/scalesec/vulnado/CommentsController.java
+++ src/main/java/com/scalesec/vulnado/CommentsController.java
@@ -13,41 +13,57 @@ public class CommentsController {
   @Value("${app.secret}")
   private String secret;
 
-  @CrossOrigin(origins = "*")
-  @RequestMapping(value = "/comments", method = RequestMethod.GET, produces = "application/json")
+    @CrossOrigin
+    @GetMapping(value = "/comments", produces = "application/json")
   List<Comment> comments(@RequestHeader(value="x-auth-token") String token) {
     User.assertAuth(secret, token);
     return Comment.fetch_all();
   }
 
-  @CrossOrigin(origins = "*")
-  @RequestMapping(value = "/comments", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
-  Comment createComment(@RequestHeader(value="x-auth-token") String token, @RequestBody CommentRequest input) {
-    return Comment.create(input.username, input.body);
+    @CrossOrigin
+    @PostMapping(value = "/comments", produces = "application/json", consumes = "application/json")
+    Comment createComment(@RequestHeader(value="x-auth-token") String token, @RequestBody CommentRequest input) {
+        return Comment.create(input.getUsername(), input.getBody());
   }
 
-  @CrossOrigin(origins = "*")
-  @RequestMapping(value = "/comments/{id}", method = RequestMethod.DELETE, produces = "application/json")
+    @CrossOrigin
+    @DeleteMapping(value = "/comments/{id}", produces = "application/json")
   Boolean deleteComment(@RequestHeader(value="x-auth-token") String token, @PathVariable("id") String id) {
     return Comment.delete(id);
   }
 }
 
 class CommentRequest implements Serializable {
-  public String username;
-  public String body;
-}
+    private String username;
+    private String body;
 
+}
+    public String getUsername() {
+
+        return username;
 @ResponseStatus(HttpStatus.BAD_REQUEST)
+    }
 class BadRequest extends RuntimeException {
+
   public BadRequest(String exception) {
+    public void setUsername(String username) {
     super(exception);
+        this.username = username;
   }
+    }
 }
 
+
+    public String getBody() {
 @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+        return body;
 class ServerError extends RuntimeException {
+    }
   public ServerError(String exception) {
+
     super(exception);
+    public void setBody(String body) {
   }
+        this.body = body;
 }
+    }
