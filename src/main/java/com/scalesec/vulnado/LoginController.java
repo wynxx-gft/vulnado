0cf30d2bcde1c2cb51959a79ba03146f1d65eb85
Julio Arruda <julio.arruda@outlook.com>
2026-02-02 15:10:04 -0300

    [GFTCodeFixer]: Update on src/main/java/com/scalesec/vulnado/LoginController.java

--- src/main/java/com/scalesec/vulnado/LoginController.java
+++ src/main/java/com/scalesec/vulnado/LoginController.java
@@ -1,10 +1,8 @@
 package com.scalesec.vulnado;
 
-import org.springframework.boot.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
 import org.springframework.beans.factory.annotation.*;
 import java.io.Serializable;
 
@@ -14,11 +12,11 @@ public class LoginController {
   @Value("${app.secret}")
   private String secret;
 
-  @CrossOrigin(origins = "*")
-  @RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+  @CrossOrigin(origins = "${app.cors.origins:http://localhost}")
+  @PostMapping(value = "/login", produces = "application/json", consumes = "application/json")
   LoginResponse login(@RequestBody LoginRequest input) {
-    User user = User.fetch(input.username);
-    if (Postgres.md5(input.password).equals(user.hashedPassword)) {
+    User user = User.fetch(input.getUsername());
+    if (Postgres.md5(input.getPassword()).equals(user.hashedPassword)) {
       return new LoginResponse(user.token(secret));
     } else {
       throw new Unauthorized("Access Denied");
@@ -27,13 +25,29 @@ public class LoginController {
 }
 
 class LoginRequest implements Serializable {
-  public String username;
-  public String password;
-}
+    private String username;
+    private String password;
+        this.password = password;
+    public void setPassword(String password) {
+        return password;
+    public String getPassword() {
+        this.username = username;
+    public void setUsername(String username) {
+
+    }
+        return username;
+    public String getUsername() {
+
 
 class LoginResponse implements Serializable {
-  public String token;
-  public LoginResponse(String msg) { this.token = msg; }
+    private String token;
+        this.token = token;
+    public void setToken(String token) {
+    }
+        return token;
+    public String getToken() {
+
+    public LoginResponse(String msg) { this.token = msg; }
 }
 
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
