ea27ba74041ca1a73800889cb7823f2a9f6153e9
Julio Arruda <julio.arruda@outlook.com>
2026-07-24 09:33:03 -0300

    [GFTCodeFixer]: Update on src/main/java/com/scalesec/vulnado/LinkLister.java

--- src/main/java/com/scalesec/vulnado/LinkLister.java
+++ src/main/java/com/scalesec/vulnado/LinkLister.java
@@ -8,31 +8,56 @@ import java.util.ArrayList;
 import java.util.List;
 import java.io.IOException;
 import java.net.*;
-
+import java.util.logging.Logger;
 
 public class LinkLister {
-  public static List<String> getLinks(String url) throws IOException {
-    List<String> result = new ArrayList<String>();
-    Document doc = Jsoup.connect(url).get();
-    Elements links = doc.select("a");
-    for (Element link : links) {
-      result.add(link.absUrl("href"));
-    }
-    return result;
-  }
+  private static final Logger LOGGER = Logger.getLogger(LinkLister.class.getName());
+  private LinkLister() {}
 
-  public static List<String> getLinksV2(String url) throws BadRequest {
-    try {
-      URL aUrl= new URL(url);
-      String host = aUrl.getHost();
-      System.out.println(host);
-      if (host.startsWith("172.") || host.startsWith("192.168") || host.startsWith("10.")){
-        throw new BadRequest("Use of Private IP");
-      } else {
-        return getLinks(url);
-      }
-    } catch(Exception e) {
-      throw new BadRequest(e.getMessage());
+
+    Document doc = Jsoup.connect(url).get();
+  public static List<String> getLinks(String url) throws IOException {
+    Elements links = doc.select("a");
+    List<String> result = new ArrayList<>();
+    for (Element link : links) {
+    Document doc = Jsoup.connect(url).get();
+      result.add(link.absUrl("href"));
+    Elements links = doc.select("a");
     }
+    for (Element link : links) {
+    return result;
+      result.add(link.absUrl("href"));
   }
+    }
+
+    return result;
+  public static List<String> getLinksV2(String url) throws BadRequest {
+  }
+    try {
+
+      URL aUrl= new URL(url);
+  public static List<String> getLinksV2(String url) throws BadRequest {
+      String host = aUrl.getHost();
+    try {
+      System.out.println(host);
+      URL aUrl = new URL(url);
+      if (host.startsWith("172.") || host.startsWith("192.168") || host.startsWith("10.")){
+      String host = aUrl.getHost();
+        throw new BadRequest("Use of Private IP");
+      LOGGER.info(host);
+      } else {
+      if (host.startsWith("172.") || host.startsWith("192.168") || host.startsWith("10.")) {
+        return getLinks(url);
+        throw new BadRequest("Use of Private IP");
+      }
+      } else {
+    } catch(Exception e) {
+        return getLinks(url);
+      throw new BadRequest(e.getMessage());
+      }
+    }
+    } catch (Exception e) {
+  }
+      throw new BadRequest(e.getMessage());
 }
+    }
