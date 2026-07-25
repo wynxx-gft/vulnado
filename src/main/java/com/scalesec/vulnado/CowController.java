3fd03954bb411f00ca15173eb1e103f66687d057
Julio Arruda <julio.arruda@outlook.com>
2026-07-25 09:16:57 -0300

    [GFTCodeFixer]: Update on src/main/java/com/scalesec/vulnado/CowController.java

--- src/main/java/com/scalesec/vulnado/CowController.java
+++ src/main/java/com/scalesec/vulnado/CowController.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 @RestController
 @EnableAutoConfiguration
 public class CowController {
-    @RequestMapping(value = "/cowsay")
+    @GetMapping(value = "/cowsay")
     String cowsay(@RequestParam(defaultValue = "I love Linux!") String input) {
         return Cowsay.run(input);
     }
